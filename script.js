c90bdc65cd157578ac43976daab92c8733d91e8b
kadelcode <kadelcode@gmail.com>
2025-02-15 23:04:58 +0100

    Add variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const apiKey = "d42caa5ba49112c3a405fff8ec0572f4";
+const searchBtn = document.getElementById("searchBtn");
+const locateBtn = document.getElementById("locateBtn");
+const weatherResult = document.getElementById("weatherResult");
